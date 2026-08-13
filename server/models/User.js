8dce18abd82e63ce8c86aa03bf0a13ecb0a285d8
Aditya Singh <124382461+aditya626332@users.noreply.github.com>
2026-08-13 19:06:23 +0530

    Add User model with schema definition

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+
+    state: {
+      type: String,
+      required: true
+    },
+
+    crop: {
+      type: String,
+      required: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
